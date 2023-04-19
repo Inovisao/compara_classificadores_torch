dd97d8958b4e540cb574b41f897d3119dbf17d94
Hemerson Pistori <pistori@ucdb.br>
2023-04-19 15:50:13 -0400

    Deixei igual compara_regressores_torch

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
-# Para instalar o compara_classificadores_torch. 
+# Para instalar as dependências do compara_regresores_torch
+# COPIE E COLE OS COMANDOS ABAIXO NO TERMINAL
+# NÃO FUNCIONA RODANDO O SCRIPT (AINDA).
 # Criar um ambiente conda.
 conda create --name cla_torch
+conda activate cla_torch
 
 # Não testei com outras versões do pytorch.
 conda install pytorch==1.12.1 torchvision==0.13.1 torchaudio==0.12.1 cudatoolkit=11.6 -c pytorch -c conda-forge
 
 # O de sempre.
 conda install -c conda-forge scikit-learn pandas matplotlib numpy seaborn opencv
-pip install timm
+pip install timm tifffile omnixai
 
-pip install tifffile
-
-pip install omnixai
