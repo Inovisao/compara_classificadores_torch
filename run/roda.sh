32d0051fa8aea568e315eb06fa921585609c8a66
Hemerson Pistori <pistori@ucdb.br>
2024-05-15 09:15:30 -0400

    Variações para tornar o resultado do exemplo mais interessante

--- run/roda.sh
+++ run/roda.sh
@@ -1,13 +1,13 @@
 # CLASSIFICADORES QUE VOCÊ QUER TESTAR
 #arqs=(alexnet coat_tiny maxvit_rmlp_tiny_rw_256 vgg19 lambda_resnet26rpt_256 vit_relpos_base_patch32_plus_rpn_256 sebotnet33ts_256 lamhalobotnet50ts_256 swinv2_base_window16_256 convnext_base resnet18 ielt)
-arqs=(resnet18)
+arqs=(resnet18 vit_relpos_base_patch32_plus_rpn_256)
 
 # OPTIMIZADORES
 #opt=(adam sgd adagrad lion sam)
-opt=(sgd)
+opt=(sgd adagrad)
 
 # LEARNING RATES
-learning_rates=(0.001)
+learning_rates=(0.001 0.0001)
 
 cd ../src
 for lr in "${learning_rates[@]}"
