2c888fb48ec32818752030aed7fa431e71a4017b
Gabriel Toshio Higa <gabrieltoshio03@gmail.com>
2024-05-16 10:52:57 -0400

    Ajustando nomes de pastas no multilayer explainer

--- multilayer_explainers/run_resnet_explainer.sh
+++ multilayer_explainers/run_resnet_explainer.sh
@@ -22,14 +22,15 @@ done
 for ((i=1;i<=$ndobras;i+=1)); do
     weights_name="1_resnet18_sgd_0.001.pth"
     folds_name="fold_${i}"
-    mkdir -p ./results_gradcam_multilayer_resnet18/${folds_name}
+    mkdir -p ./results
+    mkdir -p ./results/results_gradcam_multilayer_resnet18/${folds_name}
 
     # Run the ResNet model
     echo "Running explanations for ResNet18"
     echo "Using weights: ${weights_path}/${weights_name}"
     echo "Using test data: ${dobras_dir}/${folds_name}"
 
-    python3 resnet_explainer.py -f ${i} -w ${weights_path}/${weights_name} -td ${dobras_dir}/${folds_name} -o ./results_gradcam_multilayer_resnet18/${folds_name}
+    python3 resnet_explainer.py -f ${i} -w ${weights_path}/${weights_name} -td ${dobras_dir}/${folds_name} -o ./results/results_gradcam_multilayer_resnet18/${folds_name}
 
 done
 
