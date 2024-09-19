7cf967367190d085f9dd76578de44b44f152cdd4
Hemerson Pistori <pistori@ucdb.br>
2024-09-19 11:04:29 -0400

    Ordena os resultados por f-score

---
+++ utils/ordenaResults.sh
@@ -0,0 +1,12 @@
+# Ordena o arquivo ../results_dl/results.csv pelo valor da medida-F
+
+# Extrai o cabeçalho
+echo "Dobra,LR,Otim,Arquit,F-Score" > ../results_dl/results_ordered.csv
+
+# Processa o restante do arquivo e adiciona ao arquivo de saída
+tail -n +2 ../results_dl/results.csv | 
+cut -d ',' -f 1,2,3,4,7 | 
+sort -t ',' -k5 -r >> ../results_dl/results_ordered.csv 
+
+# Exibe o resultado na forma de tabela (10 primeiras linhas apenas)
+head ../results_dl/results_ordered.csv | column -s, -t 
